a7da95c7367f37b52e0628033cf68c75d8d95486
user <user@DESKTOP-COJ9858>
2023-11-30 19:32:07 +0900

    jvnvjvhn

--- HCYHotel/src/main/java/kr/co/sist/business/manage/HotelManageDAO.java
+++ HCYHotel/src/main/java/kr/co/sist/business/manage/HotelManageDAO.java
@@ -3,6 +3,10 @@ package kr.co.sist.business.manage;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
+import kr.co.sist.business.dao.MyBatiseHandler;
+
 public class HotelManageDAO {
 
 	private static HotelManageDAO hmDAO;
@@ -20,8 +24,21 @@ public class HotelManageDAO {
 	public List<String> selectHotelList(String id){
 		List<String> list=new ArrayList<String>();
 		
+		MyBatiseHandler mbh=MyBatiseHandler.getInstance();
+		SqlSession ss=mbh.getMyBatisHandler(false);
+		list=ss.selectList(id);
+		mbh.closeHandler(ss);
 		
-		
+		if(list==null) {
+			list.add("아이디 읎다");
+		}//if
 		return list;
+	}//selectHotelList
+	public static void main(String[] arg) {
+		List<String> list=new ArrayList<String>();
+		list=HotelManageDAO.getInstance().selectHotelList("bat1120");
+		
+		System.out.println(list);
 	}
+	
 }
