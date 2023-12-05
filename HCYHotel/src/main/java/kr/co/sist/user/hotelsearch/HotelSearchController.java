787388f8aa3e017913100920d723514c629f135e
sjh <user@DESKTOP-COJ9858>
2023-12-05 23:16:06 +0900

    다시

--- HCYHotel/src/main/java/kr/co/sist/user/hotelsearch/HotelSearchController.java
+++ HCYHotel/src/main/java/kr/co/sist/user/hotelsearch/HotelSearchController.java
@@ -1,8 +1,19 @@
 package kr.co.sist.user.hotelsearch;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HotelSearchController {
+	
+	@GetMapping("/user/hotel/hotelSearch/user_hotel_search.do")
+	public String hotelMainSearch() {
+		return "user/hotel/hotelSearch/user_hotel_search"; 
+	}//hotelMain
+	
+	@GetMapping("/user/hotel/hotelSearch/user_hotel_search_map.do")
+	public String hotelMapSearch() {
+		return "user/hotel/hotelSearch/user_hotel_search_map"; 
+	}//hotelMapSearch
 
 }
